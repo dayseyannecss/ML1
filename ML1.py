ee4f6e1c5f3d7e2796cf21732fc282bb92ebe0a9
dayse.sousa@ifma.edu.br <dayse.sousa@ifma.edu.br>
2022-10-26 14:12:53 -0300

    Descrição dos dados

--- ML1.py
+++ ML1.py
@@ -1,9 +1,24 @@
 from sklearn import datasets
 import pandas as pd
+import numpy as np
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import  train_test_split
+
 iris = datasets.load_iris()
 digits = datasets.load_digits()
+print("Iris dataset")
+
+print(iris.target_names)
+print(iris.data)
+
+#IMPRIMIR DESCRIÇÃO DO DATASET IRIS
+print(iris['DESCR'])
+
+
+print("Digito dataset")
 print(digits.data)
-digits.target
-digits.images[0]
-print(digits.target)
-print(digits.images[0])
+
+#IMPRIMIR DESCRIÇÃO DO DATASET DIGITOS
+
+print(digits['DESCR'])
