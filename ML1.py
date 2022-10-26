1ba1a0cbdb87513aa59255cc08b0f45d6a6d947b
dayse.sousa@ifma.edu.br <dayse.sousa@ifma.edu.br>
2022-10-26 15:08:56 -0300

    Plotando Gráfico con Maxplotlib

--- ML1.py
+++ ML1.py
@@ -1,7 +1,8 @@
 from sklearn import datasets
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import  train_test_split
 
@@ -15,6 +16,8 @@ print(iris.data)
 #IMPRIMIR DESCRIÇÃO DO DATASET IRIS
 print(iris['DESCR'])
 
+#MPRIMIR resultado  DO DATASET IRIS
+print(iris['target'])
 
 print("Digito dataset")
 print(digits.data)
@@ -22,3 +25,24 @@ print(digits.data)
 #IMPRIMIR DESCRIÇÃO DO DATASET DIGITOS
 
 print(digits['DESCR'])
+
+#MPRIMIR resultado  DO DATASET DIGITOS
+print(digits['target'])
+
+#Dividindo o dataset IRIS
+X_trainiRIS, X_testiRIS, y_trainIRIS, y_testIRIS = train_test_split(iris["data"], iris['target'], random_state=0)
+
+print("Treino Iris: {}".format(X_trainiRIS.shape))
+print("Teste Iris: {}".format(X_testiRIS.shape))
+
+
+plt.show()
+
+#Dividindo o dataset Digito
+X_trainDig, X_testDig, y_trainDig, y_testDig = train_test_split(digits["data"], digits['target'], random_state=0)
+
+
+print("Treino digits: {}".format(X_trainDig.shape))
+print("Teste digits: {}".format(X_testDig.shape))
+
+plt.show()
